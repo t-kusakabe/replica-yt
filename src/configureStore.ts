363912b549936df2508436ba7998d95d48d79623
t-kusakabe <ttmac0704@gmail.com>
2018-12-08 12:27:21 +0900

    hot moduleに対応する

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -4,12 +4,25 @@ import {
 } from 'redux';
 import TopReducer from './modules/top';
 
+declare global {
+  interface Window {
+    store: any;
+  }
+}
+
 const createStore = () => {
-  return reduxCreateStore(
+  if (window.store) {
+    return window.store;
+  }
+
+  const store = reduxCreateStore(
     combineReducers({
       top: TopReducer
     })
   );
+  window.store = store;
+
+  return store;
 };
 
 export default createStore;
